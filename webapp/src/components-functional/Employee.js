c92b8aa2f4232038e2c2cadb97f704b5a3e3a52f
bp <bmp4tech@gmail.com>
2020-10-06 11:55:30 -0500

    added 1st functional component

---
+++ webapp/src/components-functional/Employee.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Shows from './Shows.js';
+
+const employee = ( props ) => {
+    return (
+        <div>
+            <p>Name: </p>
+            <p>Title: </p>
+            <p>Speciality: </p>
+            <p>Animals under care: </p>
+            <div>Shows:
+                {/* <Shows/> */}
+            </div>
+        </div>
+    );
+};
+
+export default employee;
---
+++ webapp/src/components-functional/Shows.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const shows = ( props ) => {
+    return (
+        <div>
+            <p>Title: </p>
+            <p>Time: </p>
+        </div>
+    );
+};
+
+export default shows;
+
